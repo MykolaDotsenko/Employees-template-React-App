82ba237d9168f9dbf93085f98da94127d53b82a8
Mykola Dotsenko <130664123+MykolaDotsenko@users.noreply.github.com>
2024-05-12 14:40:32 +0300

    addform logic

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,6 +1,30 @@
+import { Component } from'react';
+
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
+class EmployeesAddForm extends Component {
+constructor(props){
+    super(props);
+    this.state = {
+        name: '',
+        salary: ''
+    }
+}
+
+
+onValuechange = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value
+    })
+}
+
+
+
+
+render(){
+
+    const {name, salary} = this.state;
+
     return (
         <div className="app-add-form">
             <h3>Add new employees</h3>
@@ -8,16 +32,24 @@ const EmployeesAddForm = () => {
                 className="add-form d-flex">
                 <input type="text"
                     className="form-control new-post-label"
-                    placeholder="What is his name?" />                 
+                
+                    placeholder="What is his name?"
+                    name='name'
+                   value={name}
+                   onChange={this.onValuechange} />                 
                 <input type="number"
                     className="form-control new-post-label"
-                    placeholder="Salary in $?" />
+                    placeholder="Salary in $?"
+                    name='salary'
+                    value={salary}
+                    onChange={this.onValuechange} />
 
                 <button type="submit"
                         className="btn btn-outline-light">Add</button>
             </form>
         </div>
     )
+}
 }
 
 export default EmployeesAddForm;
