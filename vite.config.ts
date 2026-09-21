1cb889a8e8a77324b8de416adbd029cf8e2158f9
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-21 10:52:07 +0300

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const GITHUB_PAGES_BASE = "/daydock/";
+
 export default defineConfig(({ mode }) => ({
-  base: mode === "pages" ? "/Employees-template-React-App/" : "/",
+  base: mode === "pages" ? GITHUB_PAGES_BASE : "/",
   plugins: [react()],
 }));
